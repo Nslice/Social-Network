fbd4bf0833a52874d359bb46ce7067d93a3496f6
Talgat <tairovtalgat@gmail.com>
2021-11-06 19:45:57 +0600

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://logos-world.net/wp-content/uploads/2020/04/Huawei-Logo.png" alt="no"/>
+        </header>
+    );
+};
+
+
+export default Header;
